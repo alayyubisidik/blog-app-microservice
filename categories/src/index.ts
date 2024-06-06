5ee2c2ecd5fc9d8d1e0800ba3398cf56f46cace3
Al Ayyubi Sidik <eka03juli1980@gmail.com>
2024-06-06 14:37:26 +0700

    updated starting message

--- categories/src/index.ts
+++ categories/src/index.ts
@@ -4,7 +4,7 @@ import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
 
-    console.log("Starting up...");
+    console.log("Starting...");
 
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY must be defined");
